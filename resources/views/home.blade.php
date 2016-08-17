cedae41db950c3ad5a83bdfd2a19d3ed01b97ce1
Dylan <dylan@whitesn.net>
2016-08-18 00:45:49 +0700

    Better view for mobile phone.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>Indonesia Maimai Hourly Rating</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
     <link href="{{ url('/css/app.css') }}" rel="stylesheet" type="text/css">
     <script src="{{ url('/js/app.js') }}"></script>
